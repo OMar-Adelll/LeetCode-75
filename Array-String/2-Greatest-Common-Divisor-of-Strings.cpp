4cdb1011ccffd47ab6fd04ffd6b83251bd7ea38d
Omar Adel <OmarAdel.connect@gmail.com>
2025-12-17 13:28:12 +0200

    Day 2

---
+++ Array-String/2-Greatest-Common-Divisor-of-Strings.cpp
@@ -0,0 +1,21 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str2.substr(0, gcd(str1.size() , str2.size()));
+    }
+};
