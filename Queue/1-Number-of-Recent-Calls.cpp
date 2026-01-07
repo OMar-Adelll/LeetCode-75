fd5744de3ef4862f22e3cb8ad0dcd3acc69410d6
Omar Adel <OmarAdel.connect@gmail.com>
2026-01-07 13:40:42 +0200

    [Easy]

---
+++ Queue/1-Number-of-Recent-Calls.cpp
@@ -0,0 +1,36 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class RecentCounter
+{
+    vector<int> records;
+    int st;
+
+public:
+    RecentCounter() : st(0) {}
+
+    int ping(int t)
+    {
+        records.push_back(t);
+        while (records[st] < t - 3000)
+            st++;
+        return records.size() - st;
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
