c22fd1faf7c5145be5472b1fcc7ec6720ecc894c
Omar Adel <OmarAdel.connect@gmail.com>
2026-01-12 16:19:10 +0200

    [Medium]

---
+++ LinkedList/1-Delete-the-Middle-Node-of-a-LinkedList.cpp
@@ -0,0 +1,45 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *deleteMiddle(ListNode *head)
+    {
+        if (!head || !head->next)
+            return nullptr;
+        ListNode *fast = head, *slow = head, *prv = nullptr;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            prv = slow;
+            slow = slow->next;
+        }
+
+        prv->next = slow->next;
+        delete slow;
+
+        return head;
+    }
+};
