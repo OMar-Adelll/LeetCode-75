26417aa64f65155afac60b69dc6829c3edc89240
Omar Adel <OmarAdel.connect@gmail.com>
2026-01-15 12:18:52 +0200

    [Medium]

---
+++ BinaryTree-DFS/3-Count-Good-Nodes-in-Binary-Tree.cpp
@@ -0,0 +1,47 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int dfs(TreeNode *node, int mx)
+    {
+        if (!node)
+            return 0;
+        int good = (node->val >= mx);
+        mx = max(mx, node->val);
+
+        good += dfs(node->left, mx);
+        good += dfs(node->right, mx);
+
+        return good;
+    }
+    int goodNodes(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        return dfs(root, root->val);
+    }
+};
