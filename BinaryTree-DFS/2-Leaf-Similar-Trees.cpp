0cf4b03e83f3c25763d6e206ea719818eb657255
Omar Adel <OmarAdel.connect@gmail.com>
2026-01-15 11:52:07 +0200

    [Easy]

---
+++ BinaryTree-DFS/2-Leaf-Similar-Trees.cpp
@@ -0,0 +1,40 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void dfs(TreeNode* node, vector<int>& v)
+    {
+        if(!node) return;
+        if(!node->left && !node->right) v.push_back(node->val);
+        dfs(node->left,v),dfs(node->right,v);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> t1 , t2;
+        dfs(root1,t1),dfs(root2,t2);
+        return t1 == t2;
+    }
+};
